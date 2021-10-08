475ac9d3e7e383ac0c2e4fb8b5f5a893c42682f2
evgeniy.thinglink.com <evgeniy.thinglink.com>
2021-10-09 00:43:36 +0300

    test server work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import {LoginPage} from "./Components/LoginPage"
 
 import React from 'react'
+import Testing from "./Components/Testing";
 
 function App() {
 
   return (
     <div className="App">
       <LoginPage />
+        <Testing/>
     </div>
   );
 }
