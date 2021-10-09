180356c5e74f5d4e8d3890d5442620ddbc22af44
Ruslan <bochkovruslan01@gmail.com>
2021-10-09 22:21:19 +0300

    added bar chart

--- frontend/src/pages/DataSet/index.js
+++ frontend/src/pages/DataSet/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Table from "../../components/Table";
 import {useHistory, useLocation} from "react-router";
 import ds from '../../dataset.json'
 import PieChart from "../../components/Charts/PieChart";
+import BarChart from "../../components/Charts/BarChart";
 
 const DataSet = props => {
 
+    const [chartConfig, setChartConfig] = useState({argument:'', value: '', chart:''})
     const history = useHistory()
     const location = useLocation()
 
@@ -16,6 +18,7 @@ const DataSet = props => {
             <h1>{location.state.dataset.name}</h1>
             <Table columnNames={columnNames} rows={ds} />
             <PieChart data={ds} valueField={'salary'} argumentField={'name'}/>
+            <BarChart data={ds} valueField={'salary'} argumentField={'name'}/>
         </div>
     )
 }
