4d04e047c250763470b69af218ffd30d9608ffba
Ruslan <bochkovruslan01@gmail.com>
2021-10-09 22:20:03 +0300

    added bar chart

---
+++ frontend/src/components/Charts/BarChart/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Paper} from "@mui/material";
+import {ArgumentAxis, BarSeries, Chart, Title, ValueAxis} from "@devexpress/dx-react-chart-material-ui";
+
+const BarChart = ({data, valueField, argumentField}) => {
+    return (
+        <Paper>
+            <Chart
+                data={data}
+                >
+                <ArgumentAxis />
+                <ValueAxis max={data.length}/>
+                <BarSeries argumentField={argumentField} valueField={valueField}/>
+                {/*<Title text={'ANAL'}/>*/}
+            </Chart>
+        </Paper>
+    )
+}
+
+export default BarChart
