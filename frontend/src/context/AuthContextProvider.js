8a08d7cd743819b7edfe72fca12e8c6482b04cb8
Ruslan <bochkovruslan01@gmail.com>
2021-10-10 01:12:12 +0300

    fixed login and register

--- frontend/src/context/AuthContextProvider.js
+++ frontend/src/context/AuthContextProvider.js
@@ -1,21 +1,21 @@
 import React, {useContext, useEffect, useReducer, useState} from 'react';
-const AuthContext=React.createContext()
+
+const AuthContext = React.createContext()
 export const AuthContextProvider = ({children}) => {
 
     const [isAuth, setIsAuth] = useState({});
-    useEffect(()=>{
-        localStorage.getItem('auth')?setIsAuth(true):setIsAuth(false)
-    },[])
-    useEffect(()=>{
-        isAuth?localStorage.setItem('auth', isAuth):localStorage.setItem('auth','false')
-    },[isAuth])
+    useEffect(() => {
+        JSON.parse(localStorage.getItem('auth')) ? setIsAuth(true) : setIsAuth(false)
+    }, [])
+    useEffect(() => {
+        isAuth && localStorage.setItem('user', isAuth) //: localStorage.setItem('user', 'false')
+    }, [isAuth])
     return (
-        <AuthContext.Provider value={{isAuth,setIsAuth}}>
+        <AuthContext.Provider value={{isAuth, setIsAuth}}>
             {children}
         </AuthContext.Provider>
     );
 };
 
 
-
-export const useIsAuth=() => useContext(AuthContext)
+export const useIsAuth = () => useContext(AuthContext)
