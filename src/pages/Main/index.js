fd5ec442825fb0a1f1552444ca2ae0bd4badb317
Ruslan <bochkovruslan01@gmail.com>
2021-10-09 15:34:41 +0300

    added main page

---
+++ src/pages/Main/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import classes from './index.module.css'
+import {Button} from "@mui/material";
+import {Link} from "react-router-dom";
+
+const Main = props => {
+    return (
+        <div className={classes.main}>
+            <Link to={'/datasets'}>
+                <Button className={classes.main_button} size='large' variant='contained'>
+                    DataSets
+                </Button>
+            </Link>
+        </div>
+    )
+}
+
+export default Main
