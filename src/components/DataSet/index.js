df084f5d8eddc9040561c2e21fc5983f6b7bf0c1
Ruslan <bochkovruslan01@gmail.com>
2021-10-09 15:51:30 +0300

    hui

---
+++ src/components/DataSet/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from "@mui/material";
+
+const DataSet = props => {
+    return (
+        <div>
+            <Button>
+
+            </Button>
+        </div>
+    )
+}
+
+export default DataSet
---
+++ src/pages/DataSets/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DataSets = props => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default DataSets
