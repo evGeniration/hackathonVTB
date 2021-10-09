1ae5543ef310901e45050b8b900599f4c16bcf1e
Ruslan <bochkovruslan01@gmail.com>
2021-10-09 23:11:57 +0300

    table args optimization

--- frontend/src/components/Table/index.js
+++ frontend/src/components/Table/index.js
@@ -4,57 +4,10 @@ import {TableRow} from "@mui/material";
 import MainTableCells from "./TableComponents/MainTableCells";
 import TableWrapper from "./TableComponents/TableWrapper";
 
-const Table = ({rows, columnNames}) => {
+const Table = ({rows}) => {
 
     const [sortValue, setSortValue] = useState(null)
-
-
-    // columnNames = [
-    //     'id',
-    //     'name',
-    //     'surname',
-    //     'birthDate',
-    //     'salary'
-    // ]
-    //
-    // rows = [
-    //     {
-    //         id: 'asdfda',
-    //         name: 'Danya',
-    //         surname: 'Alyukov',
-    //         birthDate: (new Date(2002, 1, 21)).toDateString(),
-    //         salary: 10
-    //     },
-    //     {
-    //         id: 'sadamm',
-    //         name: 'Ruslan',
-    //         surname: 'Bochkov',
-    //         birthDate: (new Date(2001, 5, 15)).toDateString(),
-    //         salary: 18
-    //     },
-    //     {
-    //         id: 'kclnlj',
-    //         name: 'Evgeniy',
-    //         surname: 'Zoloedov',
-    //         birthDate: (new Date(2001, 1, 22)).toDateString(),
-    //         salary: 5
-    //     },
-    //     {
-    //         id: 'dfdada',
-    //         name: 'Vladimir',
-    //         surname: 'Mikhailov',
-    //         birthDate: (new Date(2001, 5, 10)).toDateString(),
-    //         salary: 30
-    //     },
-    //     {
-    //         id: 'aklsdf',
-    //         name: 'Artemiy',
-    //         surname: 'Yudin',
-    //         birthDate: (new Date(1999, 6, 4)).toDateString(),
-    //         salary: 8
-    //     },
-    // ]
-
+    const columnNames = Object.keys(rows[0])
     sortValue && rows.sort((row1, row2) => row1[sortValue] > row2[sortValue] ? 1 : -1)
 
     return (
--- frontend/src/pages/DataSet/index.js
+++ frontend/src/pages/DataSet/index.js
@@ -13,15 +13,12 @@ const DataSet = props => {
         'pie': (data, value, argument) => <PieChart data={data} valueField={value} argumentField={argument}/>,
         'bar': (data, value, argument) => <BarChart data={data} valueField={value} argumentField={argument}/>
     }
-    // const history = useHistory()
     const location = useLocation()
-
-    const columnNames = Object.keys(ds[0])
 
     return (
         <div>
             <h1>{location.state.dataset.name}</h1>
-            <Table columnNames={columnNames} rows={ds} />
+            <Table rows={ds} />
             <ChartConfig data={ds} chartConfig={chartConfig} setChartConfig={setChartConfig}/>
             {chartConfig.argument && chartConfig.value && chartConfig.chart && chartRender[chartConfig.chart](ds, chartConfig.value, chartConfig.argument)}
         </div>
